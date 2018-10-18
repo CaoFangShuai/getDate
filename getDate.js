f0c0f0ab74fd4c70d6fe2f00cdc51a95ad099543
ManBa <30796342+CaoFangShuai@users.noreply.github.com>
2018-10-18 11:56:10 +0800

    Update getDate.js

--- getDate.js
+++ getDate.js
@@ -1,26 +1,3 @@
-
-
-# 纯粹的ES6获取日期时间方法
-
-你好！ 
-
-## 重点知识
-*数组的map方法*
-	map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值。
-	map() 方法按照原始数组元素顺序依次处理元素。
-*es6箭头函数*
-	箭头函数就是函数的一种简写形式，使用括号包裹参数，跟随一个 =>，紧接着是函数体
-	代码如下：
-```javascript
-	var getPrice = function() {
-  return 4.55;
-};
- 
-// 箭头函数的形式
-var getPrice = () => 4.55;
-```
-## 废话不多说具体代码
-```javascript
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -36,4 +13,3 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-```
